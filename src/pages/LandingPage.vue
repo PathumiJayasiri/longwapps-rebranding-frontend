fd9909eac5944eb674c53d4259e38aba7f76d184
PathumiJayasiri <pathumijayasiri@gmail.com>
2025-08-19 12:04:53 +0530

    Project landing page structure and navbar

---
+++ src/pages/LandingPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-page padding>
+    <section class="q-pa-xl bg-blue text-white" style="min-height: 100vh">
+      <h1>Landing Page Works!</h1>
+      <p>This content is inside QPage and now displays correctly.</p>
+    </section>
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
